1d8300995910750ce871f9ad62dfefe4fcb7356c
Phụng Trần <44075162+phungtd@users.noreply.github.com>
2023-06-01 10:13:45 +0700

    Try 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-require ('node_modules/admin-lte/plugins/jquery/jquery.min.js')
-require ('node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js')
-require ('node_modules/admin-lte/dist/js/adminlte.min.js?v=3.2.0')
+import 'admin-lte/plugins/jquery/jquery.min'
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min'
+import 'admin-lte/dist/js/adminlte.min'
 
 createApp(App).mount('#app')
